ca0e37e9ee25de7579fd5c3e9a930a0f83991ad2
David Krajco <xkrajc23@stud.feec.vutbr.cz>
2020-03-22 13:20:55 +0100

    male upravy

--- Final.py
+++ Final.py
@@ -2,18 +2,24 @@ from random import randint
 #with open ("pokus.txt", "r") as txt_file:
 #    mesta = txt_file.read()
 
-mesta = [[0, 1, 2], [1, 0, 3], [2, 3, 0]]
-POCET_MIEST = len(mesta)
+ZOZNAM_MIEST = [[0, 1, 2], [1, 0, 3], [2, 3, 0]]
+POCET_MIEST = len(ZOZNAM_MIEST)
 ludia=[]
-for n in range (5):
-    cisla = []
-    clovek = ''
-    for n1 in range(1, POCET_MIEST + 1):
-        cisla.append(n1)
-    print(cisla)
 
-    for n2 in range (1, POCET_MIEST + 1):
-        n3=randint(0,POCET_MIEST-n2)
-        clovek=clovek+str(cisla.pop(n3))
-    ludia.append(clovek)
+def generovanie_ludi(POCET_MIEST):
+    for n in range(20):
+        cisla = []
+        clovek = []
+        mesta = []
+        for n1 in range(1, POCET_MIEST + 1):
+            cisla.append(n1)
+        print(cisla)
+
+        for n2 in range(1, POCET_MIEST + 1):
+            n3 = randint(0, POCET_MIEST - n2)
+            mesta.append(cisla[n3])
+        clovek.append(mesta)
+        ludia.append(clovek)
+    return ludia
+generovanie_ludi(POCET_MIEST)
 print(ludia)
