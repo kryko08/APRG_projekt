de0668c0a4d7cb11940acf4ce6b607e8d5c94cd4
Jan Krmela <krmela.honza@gmail.com>
2020-04-28 14:43:05 +0200

    Prepsano do AJ

--- Final.py
+++ Final.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 LIST_OF_CITIES = [[0, 1, 2, 3, 4, 5], [1, 0, 6, 7, 8, 9], [2, 6, 0, 10, 11, 12], [3, 7, 10, 0, 13, 14], [4, 8, 11, 13, 0, 15], [5, 9, 12, 14, 15, 0]]
 NUM_OF_CITIES = len(LIST_OF_CITIES)
 NUM_OF_PEOPLE = 5
-POCET_ITERACI = 20
+NUM_OF_ITERATIONS = 20
 people = []
-priemery = []
-iteracie = []
+averages = []
+iterations = []
 
 
 def people_generating():
@@ -61,7 +61,7 @@ def selection(people, NUM_OF_PEOPLE):
 
 
 
-def odstraneni_kvality(people):
+def value_remove(people):
     n = 0
     for nn in range (0, NUM_OF_PEOPLE):
         del people[n][0]
@@ -71,94 +71,94 @@ def odstraneni_kvality(people):
 
 
 def selection_final(people):
-    zuzeni_populace = []
+    population_cut = []
     n = 0
-    for x in range(NUM_OF_PEOPLE) :
-        zuzeni_populace.append([])
+    for x in range(NUM_OF_PEOPLE):
+        population_cut.append([])
     while n < NUM_OF_PEOPLE:
         random_index = randint(0, len(people) - 1)
-        random_clovek = list(people[random_index])
-        zuzeni_populace[n] = list(random_clovek[0])
+        random_person = list(people[random_index])
+        population_cut[n] = list(random_person[0])
         n += 1
     people.clear()
-    people.extend(zuzeni_populace)
+    people.extend(population_cut)
     return people
 
 
 
-def mutace(people):
-    mutace_jedinec = people.pop(randint(0, NUM_OF_PEOPLE - 1))
-    print("tohle je zmutovany jedinec PRED MUTACI", mutace_jedinec)
-    poloha_prvek1 = randint(0, len(mutace_jedinec)-1)
-    poloha_prvek2 = randint(0, len(mutace_jedinec)-1)
+def mutation(people):
+    mutated_person = people.pop(randint(0, NUM_OF_PEOPLE - 1))
+    print("tohle je zmutovany jedinec PRED MUTACI", mutated_person)
+    element_position1 = randint(0, len(mutated_person)-1)
+    element_position2 = randint(0, len(mutated_person)-1)
     while True:
-        if poloha_prvek1 == poloha_prvek2:
-            poloha_prvek2 = randint(0, len(mutace_jedinec)-1)
+        if element_position1 == element_position2:
+            element_position2 = randint(0, len(mutated_person)-1)
         else:
             break
-    prvek = mutace_jedinec[poloha_prvek1]
-    mutace_jedinec[poloha_prvek1] = mutace_jedinec[poloha_prvek2]
-    mutace_jedinec[poloha_prvek2] = prvek
-    people.extend([mutace_jedinec])
-    print("tohle je mutovany jedinec PO MUTACI", mutace_jedinec)
+    element = mutated_person[element_position1]
+    mutated_person[element_position1] = mutated_person[element_position2]
+    mutated_person[element_position2] = element
+    people.extend([mutated_person])
+    print("tohle je mutovany jedinec PO MUTACI", mutated_person)
     return people
 
 
 
-def krizeni(people):
-    list_na_shuffle = []
-    nahodne_cislo = randint(0, NUM_OF_PEOPLE - 1)
-    zkrizeny_jedinec = people[nahodne_cislo]
-    print("tohle je random jedinec ke krizeni" , zkrizeny_jedinec)
-    random_index = randint(0, len(zkrizeny_jedinec)-3)
-    prvni_cislo = zkrizeny_jedinec.pop(random_index)
-    druhe_cislo = zkrizeny_jedinec.pop(random_index)
-    treti_cislo = zkrizeny_jedinec.pop(random_index)
-    list_na_shuffle.append(prvni_cislo)
-    list_na_shuffle.append(druhe_cislo)
-    list_na_shuffle.append(treti_cislo)
-    random.shuffle(list_na_shuffle)
-    for i in reversed(list_na_shuffle):
-        zkrizeny_jedinec.insert(random_index, i)
-    print("zkrizeny jedinec po krizeni", zkrizeny_jedinec)
+def hybridization(people):
+    shuffle_list = []
+    random_number = randint(0, NUM_OF_PEOPLE - 1)
+    hybrid = people[random_number]
+    print("tohle je random jedinec ke krizeni" , hybrid)
+    random_index = randint(0, len(hybrid)-3)
+    first_number = hybrid.pop(random_index)
+    second_number = hybrid.pop(random_index)
+    third_number = hybrid.pop(random_index)
+    shuffle_list.append(first_number)
+    shuffle_list.append(second_number)
+    shuffle_list.append(third_number)
+    random.shuffle(shuffle_list)
+    for i in reversed(shuffle_list):
+        hybrid.insert(random_index, i)
+    print("zkrizeny jedinec po krizeni", hybrid)
     for i in range(0, len(people)):
-        nazev = []
-        nazev.append(people.pop(0))
-        people.append(nazev)
+        name = []
+        name.append(people.pop(0))
+        people.append(name)
     return people
 
 
 
-def prumerna_kvalita(people):
-    prumer = 0
+def avg_value(people):
+    average = 0
     for i in range(0, len(people)):
-        prumer = prumer + people[i][0]
-    prumer = prumer / NUM_OF_PEOPLE
-    priemery.append(prumer)
-    return prumer
+        average = average + people[i][0]
+    average = average / NUM_OF_PEOPLE
+    averages.append(average)
+    return average
 
 
 
 def main(LIST_OF_CITIES):
     people_generating()
-    iterace = 0
-    while iterace <= POCET_ITERACI:
+    iteration = 0
+    while iteration <= NUM_OF_ITERATIONS:
         value(people, LIST_OF_CITIES)
-        print("Toto je seznam lidi po %s iteraci"%iterace, people)
-        print("Toto je prumerna kvalita:", prumerna_kvalita(people))
+        print("Toto je seznam lidi po %s iteraci"%iteration, people)
+        print("Toto je prumerna kvalita:", avg_value(people))
         quality_sorting(people)
         selection(people, NUM_OF_PEOPLE)
-        odstraneni_kvality(people)
+        value_remove(people)
         selection_final(people)
-        mutace(people)
-        krizeni(people)
-        iteracie.append(iterace)
-        iterace += 1
+        mutation(people)
+        hybridization(people)
+        iterations.append(iteration)
+        iteration += 1
     value(people, LIST_OF_CITIES)
     print('toto su finalny ludia: ', people)
-    plt.plot(iteracie, priemery)
-    plt.ylabel('Priemerna hodnota iterace')
-    plt.xlabel('iterace')
+    plt.plot(iterations, averages)
+    plt.ylabel('Avg. value')
+    plt.xlabel('Iterations')
     plt.show()
 
 
